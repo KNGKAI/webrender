6d01d2eb85f9389994c66d83dbc445b56b5228f4
KNGKAI <niel.jordaan2@gmail.com>
2020-03-16 13:47:22 +0200

    local three.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-
+import * as three from './three/three.module.js'
 import {Level} from './level.js'
 
-var renderer = new THREE.WebGLRenderer({antialias:true});
+
+var renderer = new three.WebGLRenderer({antialias:true});
 var level = new Level();
 
 renderer.setClearColor("#2F2F2F");
@@ -9,9 +10,9 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
-var cube = new THREE.Mesh( geometry, material );
+var geometry = new three.BoxGeometry( 1, 1, 1 );
+var material = new three.MeshBasicMaterial( { color: "#433F81" } );
+var cube = new three.Mesh( geometry, material );
 
 function start(){
     level.add(cube);
--- js/level.js
+++ js/level.js
@@ -1,10 +1,12 @@
+import * as three from './three/three.module.js'
+
 export class Level {
     constructor() {
         this.name = "untitled";
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+        this.camera = new three.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
         this.camera.position.z = 4;
 
-        this.scene = new THREE.Scene();
+        this.scene = new three.Scene();
     }
     
     add(object)
