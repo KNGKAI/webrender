dd21cfb59753b566bf4b2598771c4a55aa97cbc8
Niel Jordaan <nielj@imrsnt.com>
2020-03-22 17:37:44 +0200

    669

--- js/main.js
+++ js/main.js
@@ -11,36 +11,46 @@ var scene = new three.Scene();
 
 var camera = new three.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-camera.position.x = -5;
-camera.position.y = 2;
-camera.position.z = -5;
+var cube;
 
-camera.rotateY(three.Math.degToRad(225));
+function start() {
+    var gridRadius = 2;
+    gridRadius *= 1;
 
-function start(){
-    var geometry = new three.BoxGeometry( 1, 1, 1 );
-    var material = new three.MeshStandardMaterial( { color: "#ffffff" } );
-    var cube = new three.Mesh( geometry, material );
+    var geo = new three.BoxGeometry(gridRadius, gridRadius, gridRadius);
+    var mat = new three.MeshStandardMaterial({color: '#ffffff'});
+
+    cube = new three.Mesh(geo, mat);
 
     scene.add(cube);
+
+    camera.position.x = -5;
+    camera.position.y = 2;
+    camera.position.z = -5;
+
+    camera.rotateY(three.Math.degToRad(225));
+
+    var light = new three.DirectionalLight("#ffffff", 0.5)
+
+    light.position.x = -0.4;
+    light.position.y = 0.6;
+    light.position.z = -0.3;
+
+    scene.add(light)
 }
 
-function update() {
-    //
+var rotate = three.Math.degToRad(10)
+
+function update(delta) {
+    cube.rotation.y += rotate * delta
 }
 
 function render() {
     requestAnimationFrame( render );
     renderer.render(scene, camera);
-    
-    update();
-}
 
-console.log("Start")
+    update(0.1);
+}
 
 start();
 render();
-
-export {scene, camera};
-
-console.log("End")
